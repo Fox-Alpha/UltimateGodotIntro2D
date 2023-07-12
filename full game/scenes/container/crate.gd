5c7db797b69807204c7dedeb7b18c61bf1a885ab
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-07-12 13:22:47 +0200

    Chg: Add key 'E' to open crates

--- full game/scenes/container/crate.gd
+++ full game/scenes/container/crate.gd
@@ -1,5 +1,9 @@
 extends ItemContainer
 
+@onready var label = $Label
+
+var mouseover : bool = false
+
 func hit():
 	if not opened:
 		$LidSprite.hide()
@@ -8,3 +12,28 @@ func hit():
 			var pos = $SpawnPositions.get_child(randi()%$SpawnPositions.get_child_count()).global_position
 			open.emit(pos, current_direction)
 		opened = true
+		label.visible = false
+		mouseover = false
+
+
+func _on_mouse_entered():
+	if not opened:
+		label.visible = true
+		mouseover = true
+
+
+func _on_mouse_exited():
+	if not opened:
+		label.visible = false
+		mouseover = false
+		
+func _unhandled_input(event : InputEvent):
+	
+#	print(event.as_text())dddds
+	
+	if mouseover:
+		if event.is_action_pressed("interact"):
+			hit()
+
+func _on_input_event(_viewport, _event, _shape_idx):
+	pass # Replace with function body.
