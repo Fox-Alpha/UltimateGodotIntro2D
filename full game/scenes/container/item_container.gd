5b1a72877c195d84b023ed535626317d2f56ee70
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-07-12 15:52:05 +0200

    Rem: Direction, because for example crates in House are not rotatet but the house

--- full game/scenes/container/item_container.gd
+++ full game/scenes/container/item_container.gd
@@ -2,7 +2,7 @@ extends StaticBody2D
 class_name ItemContainer
 @onready var output : Marker2D = $Output
 
-@onready var current_direction: Vector2 = Vector2.ZERO #.rotated(rotation)
+#@onready var current_direction: Vector2 = Vector2.ZERO #.rotated(rotation)
 var opened: bool = false
 signal open(pos, direction)
 
