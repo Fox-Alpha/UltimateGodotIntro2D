bb86a8ce7e0d2c9bbbfca8adcf6938cab8a0c233
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-07-12 15:50:26 +0200

    Add: Add Marker to control the output of Items

--- full game/scenes/container/item_container.gd
+++ full game/scenes/container/item_container.gd
@@ -1,7 +1,8 @@
 extends StaticBody2D
 class_name ItemContainer
+@onready var output : Marker2D = $Output
 
-@onready var current_direction: Vector2 = Vector2.DOWN.rotated(rotation)
+@onready var current_direction: Vector2 = Vector2.ZERO #.rotated(rotation)
 var opened: bool = false
 signal open(pos, direction)
 
