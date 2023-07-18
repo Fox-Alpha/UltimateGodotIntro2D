d1250ed1dfa579f2b2533cc7fec052a3ba4fce83
Fox-Alpha <fox-alpha@tarnhoerner.de>
2023-07-18 12:10:18 +0200

    Add: Add a sprite for debugging the output direction Chg: Change the outputdirection to the marker2d Rem: Direction, because for example crates in House are not rotatet but the house Add: Add Marker to control the output of Items Chg: Add key 'E' to open crates ===: Related to issue #3

--- full game/scenes/container/toilet.gd
+++ full game/scenes/container/toilet.gd
@@ -1,9 +1,31 @@
 extends ItemContainer
 
+@onready var label = $Label
+
+var mouseover : bool = false
+
 func hit():
 	if not opened:
 		$LidSprite.hide()
 		$AudioStreamPlayer2D.play()
 		var pos = $SpawnPositions/Marker2D.global_position
-		open.emit(pos, current_direction)
+		open.emit(pos, output.global_position)
 		opened = true
+		label.visible = false
+		mouseover = false
+
+func _on_mouse_entered():
+	if not opened:
+		label.visible = true
+		mouseover = true
+
+
+func _on_mouse_exited():
+	if not opened:
+		label.visible = false
+		mouseover = false
+
+func _unhandled_input(event : InputEvent):
+	if mouseover:
+		if event.is_action_pressed("interact"):
+			hit()
